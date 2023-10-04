d88bc387c413cfc96afd7a661feee91e7b061f30
MarkFalcutila <83932128+markfalcutila@users.noreply.github.com>
2023-10-04 17:59:19 +0800

    version1

---
+++ assets/js/button-to-top.js
@@ -0,0 +1,36 @@
+let mybutton = document.getElementById("btn-back-to-top");
+let header = document.getElementById("header-top-side");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+    header.classList.add("add-shadow");
+    // header.style.border = "1px solid red";
+
+  } else {
+    mybutton.style.display = "none";
+    header.classList.remove("add-shadow");
+
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  $('html, body').animate({
+    scrollTop: 0
+}, 500); 
+  
+}
+
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// document.querySelectorAll(".nav-link").forEach((link) => {
+//     if (link.href === window.location.href) {
+//         link.classList.add("menu-active");
+//         link.setAttribute("aria-current", "page");
+//     }
+// });
+
+
+
+// mybutton.addEventListener("click", backToTop);
+
+// function backToTop() {
+//   $('html, body').animate({
+//     scrollTop: 0
+// }, 500); 
+  
+// }
+
+
