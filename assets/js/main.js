b22992d4e49cb5c3be2c74e9e77953a9ae5aef79
MarkFalcutila <83932128+markfalcutila@users.noreply.github.com>
2023-11-16 17:53:34 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,83 @@
 //         video.pause();
 //     });
 // });
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let burger = document.getElementById('burger');
+    let nav = document.getElementById('navbar');
+    let navLinks = document.querySelectorAll('.nav-links');
+    let bgdark = document.getElementById('bg-container');
+
+    let svcbtn = document.getElementById('serviceTab');
+    let tabLeft = document.getElementById('tablistLeft');
+
+
+    burger.addEventListener('click', function(e){
+        this.classList.toggle('is-open');
+        nav.classList.toggle('open-navbar');
+        bgdark.classList.toggle('bg-open-container');
+        // console.log("clicked burger menu")
+    })
+
+
+    svcbtn.addEventListener('click', function(e){
+        tabLeft.classList.toggle('open-lefttab');
+        // console.log("button for closing tab")
+    });
+
+
+
+
+    document.body.addEventListener('click', function () {
+        // Close the navbar when clicking outside of it
+        if(!nav.contains(event.target) && !burger.contains(event.target) ){
+            burger.classList.remove('is-open');
+            nav.classList.remove('open-navbar');
+            bgdark.classList.remove('bg-open-container');
+        }
+
+        if(!svcbtn.contains(event.target) && !tabLeft.contains(event.target) ){
+            tabLeft.classList.remove('open-lefttab');
+        }
+      
+    });
+
+
+     // Add event listeners to close the navbar when clicking on a nav link
+     navLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+
+            e.stopPropagation();
+
+            burger.classList.remove('is-open');
+            nav.classList.remove('open-navbar');
+            console.log("navlinks clicked")
+
+            navLinks.forEach(function (navLink) {
+                // navLink.classList.remove('active');
+                bgdark.classList.remove('bg-open-container');
+
+            });
+
+            // this.classList.add('active');
+
+        });
+    });
+
+    // Function to check if the clicked element is a nav link or its descendant
+    // function isNavLink(element) {
+    //     let currentElement = element;
+    //     while (currentElement) {
+    //         if (currentElement.classList && currentElement.classList.contains('nav-link')) {
+    //             return true;
+    //         }
+    //         currentElement = currentElement.parentElement;
+    //     }
+    //     return false;
+    // }
+
+
+   
+
+});
